08fc8566f64868893f09b3af24d45e077466c85c
Geon_27 <yukgeon98527@gmail.com>
2023-03-06 00:03:09 +0900

    23/03/06 viewcontroller added

--- Delegate-imagePicker/ViewController.swift
+++ Delegate-imagePicker/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    @IBAction func pick(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
