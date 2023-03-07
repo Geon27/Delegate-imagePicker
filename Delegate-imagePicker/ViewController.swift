374025470059b9359b460f9cad15269cdd03a642
Geon_27 <yukgeon98527@gmail.com>
2023-03-07 19:07:08 +0900

    23/03/07 added

--- Delegate-imagePicker/ViewController.swift
+++ Delegate-imagePicker/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
     @IBAction func pick(_ sender: Any) {
+    // 이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 On
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
+    
+    // 이미지 피커 컨트롤러 실행
+    self.present(picker, animated: false)
 }
 
