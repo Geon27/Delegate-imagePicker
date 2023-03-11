88bd2101aa96e85e488722c6e7f09ba5b6e8e827
Geon_27 <yukgeon98527@gmail.com>
2023-03-11 11:38:14 +0900

    23/03/11 added

--- Delegate-imagePicker/ViewController.swift
+++ Delegate-imagePicker/ViewController.swift
@@ -26,9 +26,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         // 이미지 피커에서 이미지를 선택하지 않고 취소할때 호출되는 메소드
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         }
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false)
+        
+        // 알림창 호출
+        let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        self.present(alert, animated: false)
+    }
         // 이미지 피커에서 이미지를 선택했을때 호출되는 메소드
         func imagePickerController(_ picker: UIImagePickerController, didFinish
                                    PickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         }
     }
-}
